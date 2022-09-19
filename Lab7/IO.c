636b216a1154813f24c3c98f67015aea3b03ec12
Yang Tsung-Hsien <101848951+YangTsung-Hsien@users.noreply.github.com>
2022-09-19 02:49:38 +0200

    Update IO.c
    complete IO_Init , IO_HeartBeat and IO_Touch function

--- Lab7/IO.c
+++ Lab7/IO.c
@@ -5,12 +5,13 @@
 // Runs on TM4C123
 // Program written by: Yang Tsung-Hsien
 // Date Created: 31.07.2022
-// Last Modified:  01.08.2022
+// Last Modified:  19.09.2022
 // Lab number: 7
 
 
 #include "../inc/tm4c123gh6pm.h"
 #include <stdint.h>
+#include "ST7735.h"
 
 //------------IO_Init------------
 // Initialize GPIO Port for a switch and an LED
@@ -18,7 +19,9 @@
 // Output: none
 void IO_Init(void) {
  // --UUU-- Code to initialize PF4 and PF2
-	
+	volatile unsigned long delay;
+	SYSCTL_RCGCGPIO_R |= 0x20;
+	delay = SYSCTL_RCGCGPIO_R;
 	GPIO_PORTF_AMSEL_R &= 0x00; // disable analog function
 	GPIO_PORTF_PCTL_R &= 0x00000000; // GPIO clear bit PCTL
 	GPIO_PORTF_DIR_R &= ~0x10; // PF4 input
@@ -27,8 +30,8 @@ void IO_Init(void) {
 	GPIO_PORTF_PUR_R |= 0x10; // enable pull up resistor on PF4
 	GPIO_PORTF_DEN_R |= 0x14; //enable digital pins on PF2 & PF4
 	
-	unsigned long sw1 = GPIO_PORTF_DATA_R & 0x10;  // read PF4 
-	unsigned long LED = GPIO_PORTF_DATA_R |= 0x04; // PF2 on
+	//unsigned long sw1 = GPIO_PORTF_DATA_R & 0x10;  // read PF4 
+	//unsigned long LED = GPIO_PORTF_DATA_R |= 0x04; // PF2 on
 	
 }
 
@@ -38,6 +41,8 @@ void IO_Init(void) {
 // Output: none
 void IO_HeartBeat(void) {
  // --UUU-- PF2 is heartbeat
+	
+	GPIO_PORTF_DATA_R ^= 0x04;
 }
 
 
@@ -48,5 +53,20 @@ void IO_HeartBeat(void) {
 // Output: none
 void IO_Touch(void) {
  // --UUU-- wait for release; delay for 20ms; and then wait for press
+	while(1)
+	{
+		if( (GPIO_PORTF_DATA_R & 0x10) == 0)
+			break;
+	}
+	
+	Delay1ms(20);
+	
+	while(1)
+	{
+		if((GPIO_PORTA_AHB_DATA_R & 0x10) != 0)
+			return;
+	
+	}
+	
 }  
 
